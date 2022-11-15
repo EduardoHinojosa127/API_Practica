a84633d4ebd7c54c97fe1d15f18b9eaa06379f40
EduardoHinojosa127 <eduardo.hinojosa@tecsup.edu.pe>
2022-11-15 11:53:35 -0500

    Practica-3

--- API_Practica/API_Practica/settings.py
+++ API_Practica/API_Practica/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'HOST' : 'localhost',
         "PORT": "3306",
         "USER": "root",
-        "PASSWORD":"gatoronpedo123",
+        "PASSWORD":"",
         "NAME":"api_practica",
         "OPTIONS":{
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
